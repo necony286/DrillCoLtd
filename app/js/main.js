b076a2a48ad07afab97356fb76c3bd69cddf8436
Necony (Nemanja Bojovic) <Necony286@gmail.com>
2025-08-18 01:02:51 +0200

    styling | random slider images added

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,15 @@
 // Main application scripts
 /* global Splide */
 
-document.addEventListener('DOMContentLoaded', () => {
-  const slider = document.querySelector('#main-slider');
+document.addEventListener("DOMContentLoaded", () => {
+  const slider = document.querySelector("#main-slider");
   if (slider) {
     new Splide(slider, {
-      type: 'loop',
+      type: "loop",
       autoplay: true,
       arrows: false,
       pagination: false,
+      cover: true,
     }).mount();
   }
 });
